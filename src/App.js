f83650ba149204e67680d66aea369abff27e3dea
Yurev-Sergey <87377996+SergeyDibo@users.noreply.github.com>
2024-05-17 16:35:31 +0300

    comment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import './scss/app.scss';
+import './scss/app.scss'; // Importing the app's SCSS file
 
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from "./components/PizzaBlock";
 
+// App component is the main component that renders the application
 function App() {
     return (
-        <div className="wrapper">
+        <div className="wrapper"> {/* Wrapper div to wrap the entire content */}
             <Header/>
-            <div className="content">
-                <div className="container">
-                    <div className="content__top">
-                        <Categories/>
-                        <Sort/>
+            <div className="content"> {/* Content div that contains the main application content */}
+                <div className="container"> {/* Container div to apply consistent styling */}
+                    <div className="content__top"> {/* Top section of the content */}
+                        <Categories/> {/* Component to render categories */}
+                        <Sort/> {/* Component to render sorting options */}
                     </div>
-                    <h2 className="content__title">Все пиццы</h2>
-                    <div className="content__items">
-                        <PizzaBlock title={"Мексиканская"} prise={500}/>
-                        <PizzaBlock title={"eee"} prise={333}/>
-                        <PizzaBlock/>
+                    <h2 className="content__title">Все пиццы</h2> {/* Title for the 'All Pizzas' section */}
+                    <div className="content__items"> {/* Div to contain all the PizzaBlock components */}
+                        <PizzaBlock title={"Мексиканская"} prise={500}/> {/* PizzaBlock component with title and price */}
+                        <PizzaBlock title={"eee"} prise={333}/> {/* PizzaBlock component with title and price */}
+                        <PizzaBlock/> {/* PizzaBlock component without any props */}
                     </div>
                 </div>
             </div>
