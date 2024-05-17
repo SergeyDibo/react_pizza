7d6bef74c5773f166ee3427387ce226911707404
Yurev-Sergey <87377996+SergeyDibo@users.noreply.github.com>
2024-05-17 16:35:30 +0300

    comment

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+// Import the React library and the ReactDOM module
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Import the App component from the App.js file
 import App from './App';
 
-
+// Create a constant named `root` that is assigned the result of calling the
+// `ReactDOM.createRoot` method on the element with the id of 'root' in the HTML document
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Call the `render` method on the `root` constant, passing in the JSX code for the
+// App component wrapped in a React.StrictMode component
 root.render(
   <React.StrictMode>
     <App />
